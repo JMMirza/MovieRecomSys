4c95d4f88d7f00ab6a906fdcec2debd9eb36c444
jmmirza <janmuhammadmirza@gmail.com>
2021-12-24 19:40:57 +0500

    watch later

--- routes/routes.js
+++ routes/routes.js
@@ -35,7 +35,7 @@ const twoFAMiddleware = require('../middleware/twoFAMiddleware')
 
 router.post('/likemovie', auth, likeMovies);
 router.post('/listlikemovie', auth, listLikemovies);
-router.post('/watched', auth, watchLater);
+router.post('/watchLater', auth, watchLater);
 router.post('/signup', [signupMiddleware, age, checkEmail], signUp);
 router.post('/signin', signinMiddleware, simpleSignIn);
 router.post('/user/tvalidate', [twoFAMiddleware, auth], twoFA)
